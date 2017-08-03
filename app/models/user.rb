b27454a92e6239dfc8331cc5e5d80e87cbdcd650
Riki <rick.libby.91@gmail.com>
2017-08-03 16:58:54 +0000

    created validation. doesn't quite work yet, but moving forward. gonna come back

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,19 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true,
-                            uniqueness: true
+  validates :email, presence: true
+  before_create :email_must_not_exist
 
   has_secure_password
+
+  private
+
+  def email_must_not_exist
+    @user = User.find_by_email(email)
+    if @user.present?
+      errors.add(:email, 'already exists')
+      throw(:abort)
+    end
+  end
 
 end
